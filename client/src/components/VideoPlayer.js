86f6a7f61311d9c58c664b72b5371f9abfeaad74
dean-kim <keunu07@gmail.com>
2016-10-10 18:49:14 +0900

    done

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -3,9 +3,9 @@ import React from 'react';
 class VideoPlayer extends React.Component{
   render(){
     return (
-      <div>
-        <h1>Video Player</h1>
-      </div>
+		<div className="video-player">
+        <iframe width="432" height="243" className="embed-responsive-item" src={"https://www.youtube.com/embed/"+this.props.video.id.videoId} allowFullScreen></iframe>
+     </div>
     )
   };
 };
